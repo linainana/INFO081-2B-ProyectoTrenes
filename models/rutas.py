5c5cb722c43d00c19e73839e9e00d7bad766671a
nissydva <escarlett.vargas.a@gmail.com>
2025-11-08 18:10:10 -0300

    Update rutas.py

--- models/rutas.py
+++ models/rutas.py
@@ -1,1 +1,8 @@
+class Ruta:
+    def __init__(self, origen, destino, distancia):
+        self.origen = origen
+        self.destino = destino
+        self.distancia = distancia
 
+    def descripcion_ruta(self):
+        return f"{self.origen} → {self.destino} ({self.distancia} km)"
