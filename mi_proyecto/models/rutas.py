a74fa8b0980f2b73246e171b8cd02d38d460f629
Cata <cata.provoste.c@gmail.com>
2025-12-02 00:26:08 -0300

    Update rutas.py

--- mi_proyecto/models/rutas.py
+++ mi_proyecto/models/rutas.py
@@ -1,8 +1,24 @@
 class Ruta:
-    def __init__(self, origen, destino, distancia):
-        self.origen = origen
-        self.destino = destino
-        self.distancia = distancia
+    def __init__(self, id_, estaciones, distancia_total=0):
+        """
+        id_ : identificador de la ruta
+        estaciones : lista de objetos Estacion en orden
+        distancia_total :  distancia total
+        """
+        self.id = id_
+        self.estaciones = estaciones
+        self.distancia_total = distancia_total
 
     def descripcion_ruta(self):
-        return f"{self.origen} → {self.destino} ({self.distancia} km)"
+        nombres = "->".join([e.nombre for e in self.estaciones])
+        return f"{nombres} ({self.distancia_total} km)"
+
+    def obtener_siguiente(self, estacion_actual):
+        """Devuelve la siguiente estación en la ruta"""
+        if estacion_actual in self.estaciones:
+            i = self.estaciones.index(estacion_actual)
+            if i + 1 < len(self.estaciones):
+                return self.estaciones[i+1]
+        return None
+
+        
